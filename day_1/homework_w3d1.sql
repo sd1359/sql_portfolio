90c8efc6968b3b2864aefaef6d5136d0931d1403
Stephanie Duncan <74064185+stephanieduncan@users.noreply.github.com>
2021-03-15 13:32:22 +0000

    Update homework_w3d1.sql

--- day_1/homework_w3d1.sql
+++ day_1/homework_w3d1.sql
@@ -89,6 +89,83 @@ SELECT
 FROM employees 
 GROUP BY department
 
+           /* 13. Do a count by year of the start_date of all employees, ordered most recent year last. */
+
+SELECT 
+  EXTRACT(YEAR FROM start_date) AS year, 
+  COUNT(id) AS num_employees_started 
+FROM employees 
+GROUP BY EXTRACT(YEAR FROM start_date) 
+ORDER BY year ASC NULLS LAST
+
+
+/* 14. Return the first_name, last_name and salary of all employees together with a new column called salary_class with a value 'low' where salary is less than 40,000 and value 'high' where salary is greater than or equal to 40,000. */
+
+SELECT 
+  first_name, 
+  last_name, 
+  CASE 
+    WHEN salary < 40000 THEN 'low'
+    WHEN salary IS NULL THEN NULL
+    ELSE 'high' 
+  END AS salary_class
+FROM employees
+
+/* 15. The first two digits of the local_sort_code (e.g. digits 97 in code 97-09-24) in the pay_details table are indicative of the region of an account. Obtain counts of the number of pay_details records bearing each set of first two digits? Make sure that the count of NULL local_sort_codes comes at the top of the table, and then order all subsequent rows first by counts in descending order, and then by the first two digits in ascending order */
+
+SELECT
+  SUBSTRING(local_sort_code, 1, 2) AS first_two_digits,
+  COUNT(id) AS count_records
+FROM pay_details
+GROUP BY SUBSTRING(local_sort_code, 1, 2)
+ORDER BY 
+  CASE 
+    WHEN SUBSTRING(local_sort_code, 1, 2) IS NULL THEN 1
+    ELSE 0
+  END DESC,
+  count_records DESC,
+  first_two_digits ASC
+  
+  /* Second Method */
+  
+  SELECT
+  first_two_digits,
+  count_records
+FROM (
+  SELECT
+    CASE 
+      WHEN SUBSTRING(local_sort_code, 1, 2) IS NULL THEN 1
+      ELSE 0
+    END AS is_null,
+    SUBSTRING(local_sort_code, 1, 2) AS first_two_digits,
+    COUNT(id) AS count_records
+  FROM pay_details
+  GROUP BY SUBSTRING(local_sort_code, 1, 2)
+) AS temp_table
+ORDER BY is_null DESC, count_records DESC, first_two_digits ASC
+
+SELECT
+  first_two_digits,
+  count_records
+FROM (
+  SELECT
+    SUBSTRING(local_sort_code, 1, 2) IS NULL, 
+    SUBSTRING(local_sort_code, 1, 2),
+    COUNT(id)
+  FROM pay_details
+  GROUP BY SUBSTRING(local_sort_code, 1, 2)
+) temp_table(is_null, first_two_digits, count_records)
+ORDER BY is_null DESC NULLS LAST, count_records DESC, first_two_digits ASC
+
+/* 16. Return only the numeric part of the local_tax_code in the pay_details table, preserving NULLs where they exist in this column. */
+
+SELECT
+  local_tax_code,
+  REGEXP_REPLACE(local_tax_code, '\D','','g') AS numeric_part_tax_code
+FROM pay_details 
+
+
+
 
 
 
